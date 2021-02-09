f65e1c79f32f39414f43321b54de9cc2a6746447
mhmdsalti <alsalty_r@hotmail.com>
2021-02-09 10:00:27 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -8,30 +8,34 @@ console.log(userName);
 let User = prompt('What\'s your name ');
 alert('its our pleasure to have you with us MR/MRS ' + User );
 
-let question1 = prompt (' do you think I eat alot ? ')
+let question1 = prompt (' do you think I eat a lot ? ')
 
 
 switch (question1.toLowerCase()) {
-  case 'yes':
-    alert('Ops nice try!');
-    console.log('Wrong !');
-    break;
+
+ case 'yes':
+  alert('Ops nice try!');
+  console.log('Wrong !');
+  break;
+
   case 'y':
-    alert('Ops nice try!');
-    console.log('Wrong !');
-    break;
-   case 'no' :
-   alert(' Good Job !');
-   console.log('Correct !');
-break;
-case 'n' : 
-alert(' Good Job !');
-console.log('Correct !');
-break;
+  alert('Ops nice try!');
+  console.log('Wrong !');
+  break;
+
+  case 'no' :
+  alert(' Good Job !');
+  console.log('Correct !');
+  break;
+
+  case 'n' : 
+  alert(' Good Job !');
+  console.log('Correct !');
+  break;
+
   default:
-    
-    alert('you have to answer with (yes or y , no or n)');
-    break;
+  alert('you have to answer with (yes or y , no or n)');
+  break; 
   }
 
 
@@ -40,101 +44,121 @@ break;
 
 
   switch (question2.toLowerCase()) {
-    case 'yes':
-      alert('Nice try !');
-      console.log('Wrong !');
-      break;
-    case 'y':
-      alert('Nice try !');
-      console.log('Wrong !');
-      break;
-     case 'no' :
-     alert('Good Job !');
-     console.log('Correct ! ');
-     break;
+
+  case 'yes':
+  alert('Nice try !');
+  console.log('Wrong !');
+  break;
+
+  case 'y':
+  alert('Nice try !');
+  console.log('Wrong !');
+  break;
+
+  case 'no' :
+  alert('Good Job !');
+  console.log('Correct ! ');
+  break;
+
   case 'n' : 
   alert('Good Job !');
   console.log('Correct ! ');
   break;
-    default:
-      alert('you have to answer with (yes or y , no or n)');
-      break;
-    }
+
+  default:
+  alert('you have to answer with (yes or y , no or n)');
+  break;
+ }
   
 
-    let question3 = prompt('do i love my life ?');
+  let question3 = prompt('do i love my life ?');
 
-    switch (question3.toLowerCase()) {
-      case 'yes':
-        alert(' Yaay  ');
-        console.log('Correct !');
-        break;
-      case 'y':
-        alert(' Yaay  ');
-        console.log('Correct !');
-        break;
-       case 'no' :
-       alert('Nice Try ! ');
-       console.log('Wrong !');
-       break;
-    case 'n' : 
-    alert('Nice Try ! ');
-    console.log('Wrong !');
-    break;
-      default:
-        alert('you have to answer with (yes or y , no or n)');
-        break;
-      }
+  switch (question3.toLowerCase()) {
+
+  case 'yes':
+  alert(' Yaay  ');
+  console.log('Correct !');
+  break;
+
+  case 'y':
+  alert(' Yaay  ');
+  console.log('Correct !');
+  break;
+
+  case 'no' :
+  alert('Nice Try ! ');
+  console.log('Wrong !');
+  break;
+
+  case 'n' : 
+  alert('Nice Try ! ');
+  console.log('Wrong !');
+  break;
+
+  default:
+  alert('you have to answer with (yes or y , no or n)');
+  break;
+  }
     
-      let question4 = prompt('am i such a nerd  ?! ');
+  let question4 = prompt('am i such a nerd  ?! ');
 
-      switch (question4.toLowerCase()) {
-        case 'yes':
-          alert(' Semi nerd lol ! ');
-          console.log('Correct !');
-          break;
-        case 'y':
-          alert(' Semi nerd lol  ');
-          console.log('Correct !');
-          break;
-         case 'no' :
-         alert('But i am ! ');
-         console.log('Wrong !');
-         break;
-      case 'n' : 
-      alert('But i am ! ');
-      console.log('Wrong !');
-      break;
-        default:
-          alert('you have to answer with (yes or y , no or n)');
-          break;
-        }
+  switch (question4.toLowerCase()) {
 
+  case 'yes':
+  alert(' Semi nerd lol ! ');
+  console.log('Correct !');
+  break;
 
-    let question5 = prompt('am  i muslim  ?');
+  case 'y':
+  alert(' Semi nerd lol  ');
+  console.log('Correct !');
+  break;
 
-    switch (question5.toLowerCase()) {
-      case 'yes':
-        alert(' ALHAMDULLAH for sure');
-        console.log('Correct !');
-        break;
-      case 'y':
-        alert('ALHAMDULLAH for sure');
-        console.log('Correct !');
-        break;
-       case 'no' :
-       alert('ASTGHFERULLAH ');
-       console.log('Wrong !');
-       break;
-    case 'n' : 
-    alert('ASTGHFERULLAH ');
-    console.log('Wrong !');
-    break;
-      default:
-        alert('you have to answer with (yes or y , no or n)');
-        break;
-      }
+  case 'no' :
+  alert('But i am ! ');
+  console.log('Wrong !');
+  break;
+
+  case 'n' : 
+  alert('But i am ! ');
+  console.log('Wrong !');
+  break;
+
+  default:
+  alert('you have to answer with (yes or y , no or n)');
+  break;
+  }
 
 
-  alert(' Vist us Again MR/MRs ' + User);
+  let question5 = prompt('am  i muslim  ?');
+
+  switch (question5.toLowerCase()) {
+
+  case 'yes':
+  alert(' ALHAMDULLAH for sure');
+  console.log('Correct !');
+  break;
+
+  case 'y':
+  alert('ALHAMDULLAH for sure');
+  console.log('Correct !');
+  break;
+
+  case 'no' :
+  alert('ASTGHFERULLAH ');
+  console.log('Wrong !');
+  break;
+
+  case 'n' : 
+  alert('ASTGHFERULLAH ');
+  console.log('Wrong !');
+  break;
+
+  default:
+  alert('you have to answer with (yes or y , no or n)');
+  break;
+  }
+
+
+  alert(' Visit us Again MR/MRs ' + User);
  
